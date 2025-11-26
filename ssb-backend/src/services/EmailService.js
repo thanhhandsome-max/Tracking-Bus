0e73aaf7ce8f69245723ef85bb870585c18da39b
Irthn1311 <nhuutri1311@gmail.com>
2025-11-26 19:42:34 +0700

    fix gửi email

--- ssb-backend/src/services/EmailService.js
+++ ssb-backend/src/services/EmailService.js
@@ -14,7 +14,7 @@ class EmailService {
       this.transporter = nodemailer.createTransport({
         host: config.email.host,
         port: config.email.port,
-        secure: config.email.port === 465, // true for 465, false for other ports
+        secure: config.email.port === 587, // true for 465, false for other ports
         auth: {
           user: config.email.user,
           pass: config.email.password,
